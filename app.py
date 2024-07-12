96aa819108e9ed8c5f42aa0f55251ad3f3b184c4
MoneyDouble <348104201@qq.com>
2024-07-12 13:31:23 +0800

    Update app.py add celerymanager

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, DiskcacheManager, CeleryManager
 import plotly.express as px
 import pandas as pd
-#import functool
-from flask_caching import Cache
+from uuid import uuid4
+#from flask_caching import Cache
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
 app = Dash(__name__)
 server = app.server
+launch_uid = uuid4()
+TIMEOUT = 60 * 60 * 24
+
+if 'REDIS_URL' in os.environ:
+    # Use Redis & Celery if REDIS_URL set as an env variable
+    from celery import Celery
+    celery_app = Celery(__name__, broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
+    background_callback_manager = CeleryManager(
+        celery_app, cache_by=[lambda: launch_uid], expire=TIMEOUT
+    )
+
+else:
+    # Diskcache for non-production apps when developing locally
+    import diskcache
+    cache = diskcache.Cache("./cache")
+    background_callback_manager = DiskcacheManager(
+        cache, cache_by=[lambda: launch_uid], expire=TIMEOUT
+    )
 app.layout = [
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ]
 
-cache = Cache(app.server, config={
-    'CACHE_TYPE': 'filesystem',
-    'CACHE_DIR': 'cache-directory'
-})
+#cache = Cache(app.server, config={
+#    'CACHE_TYPE': 'filesystem',
+#    'CACHE_DIR': 'cache-directory'
+#})
 
-TIMEOUT = 60 * 60 * 24
-#@functools.lru_cache(maxsize=32)
+
 @callback(
     Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Input('dropdown-selection', 'value'),
+    background=True
 )
-@cache.memoize(timeout=TIMEOUT)
+#@cache.memoize(timeout=TIMEOUT)
 def update_graph(value):
     dff = df[df.country==value]
     return px.line(dff, x='year', y='pop')
