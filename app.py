ef0e928b6787a0009b939a744b343520a50b348a
franklili <348104201@qq.com>
2024-08-01 16:23:45 +0800

    Revert "add dash_auth"
    This reverts commit 75efb738346c4901b38bd10df564451cd51578f3.

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 import dash
 from dash import Dash, html, dcc
 #import dash_bootstrap_components as dbc
-import dash_auth
-import os, json
 
-
-username_password_str = os.environ.get('username_password_dict')
-#username_password_json = username_password_str.json()
-VALID_USERNAME_PASSWORD_PAIRS =json.loads(username_password_str)
 # Initialize the app - incorporate a Dash Bootstrap theme
 #external_stylesheets = [dbc.themes.SUPERHERO]
-
 app = Dash(__name__, use_pages=True)#, external_stylesheets=external_stylesheets)
 server = app.server
-auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
-
 app.layout = html.Div([
                 html.H1('翻一番比特币投资驾驶舱'),
                 html.Div([
