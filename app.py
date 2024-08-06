02a476b9e66aa1b71657fb8f221159e6bf29aca0
franklili <348104201@qq.com>
2024-08-06 10:06:14 +0800

    add external scripts

--- app.py
+++ app.py
@@ -3,25 +3,18 @@ from dash import Dash, html, dcc
 #import dash_bootstrap_components as dbc
 
 # Initialize the app - incorporate a Dash Bootstrap theme
-'''
+
 #external_stylesheets = [dbc.themes.SUPERHERO]
 # external JavaScript files
 external_scripts = [
-    'https://unpkg.com/dash-core-components@2.14.1/dash_core_components/dash_core_components-shared.js',
-    'https://unpkg.com/dash-core-components@2.14.1/dash_core_components/dash_core_components.js',
-    'https://unpkg.com/dash-daq@0.5.0/dash_daq/dash_daq.min.js',
-    'https://unpkg.com/dash-html-components@2.0.18/dash_html_components/dash_html_components.min.js',
-    'https://unpkg.com/dash-renderer@1.20.1/build/dash_renderer.min.js',
-    'https://unpkg.com/dash-table@5.2.11/dash_table/bundle.js',
-    'https://unpkg.com/prop-types@15.8.1/prop-types.min.js',
-    #'https://unpkg.com/react-dom@16.14.0/umd/react-dom.production.min.js',
-    'https://unpkg.com/react@16.14.0/umd/react.production.min.js'
+        'https://unpkg.com/dash_tvlwc@0.1.2/dash_tvlwc/dash_tvlwc.min.js'
 ]
-'''
+
 app = Dash(__name__, 
             use_pages=True,
-            serve_locally=False
-            )#, external_stylesheets=external_stylesheets)external_scripts=external_scripts
+            serve_locally=False,
+            external_scripts=external_scripts
+            )#, external_stylesheets=external_stylesheets)
 server = app.server
 app.layout = html.Div([
                 html.H1('翻一番比特币投资驾驶舱'),
