df0c6c87caf1eb46bfd87aadc553a79ac51de9c6
li li <348104201@qq.com>
2024-09-11 18:03:05 +0800

    add case page

--- pages/case.py
+++ pages/case.py
@@ -23,9 +23,9 @@ import dash_ag_grid as dag
 from dash.exceptions import PreventUpdate
 
 register_page(__name__,
-    title='7.我的分享',
-    name='7.我的分享-客户')
-require_login(__name__)
+    title='6.案例',
+    name='6.案例')
+
 app1 = get_app()
 # 创建logger
 # 创建FileHandler，并添加到logger.handlers列表
@@ -52,14 +52,10 @@ else:
     })
 
 
-def layout(**kwargs):
-    if not current_user.is_authenticated:
-        return html.Div(["请 ", dcc.Link("登录", href="/login"), " 继续查看。"])
-
-    return html.Div(
+layout = html.Div(
         [
             #dcc.Interval(id='timer', interval=500),
-            dcc.Store(id="store_12"),            
+            dcc.Store(id="store_13"),            
             html.Div(className='container', children=[
                 html.Div([
                     html.Div([
@@ -76,44 +72,11 @@ def layout(**kwargs):
                             html.Br(),
                             dcc.Link("5.比特币价格上限和下限", href="/bitcoin-upper-lower-price-client"),
                             html.Br(),
-                            dcc.Link("6.我的累计收益率", href="/my-total-return-client")
                         ])
-                        #    dcc.Link(f"{page['name']}", href=page["relative_path"])# - {page['path']}
-                        #) for page in page_registry.values()
                     ]),            
                 ]),
                 html.Br(),
-                html.Div([
-                    dcc.Input(id="input1",
-                        type="text",
-                        placeholder="公开的昵称"), 
-                    html.Div([html.Img(id='output-image-database', style={'width': '50px', 'height': '50px'})],style={'padding': 10, 'flex': 1}),
-                    dcc.Upload(
-                        id='upload-image',
-                        children=html.Div([
-                            '拖拽文件或',
-                            html.A(' 点击上传')
-                        ]),
-                        style={
-                            'width': '100%',
-                            'height': '60px',
-                            'lineHeight': '60px',
-                            'borderWidth': '1px',
-                            'borderStyle': 'dashed',
-                            'borderRadius': '5px',
-                            'textAlign': 'center',
-                            'margin': '10px'
-                        },
-                    ),
-                    html.Div([html.Img(id='output-image-upload', style={'width': '50px', 'height': '50px'})],style={'padding': 10, 'flex': 1}),
-                ]),
-                dcc.RadioItems(['不公开', '公开'],  id="radio_items"),
-                ## button
-                html.Button('  分享  ', id='button_12', n_clicks=0),
-                html.Br(),
-                html.Div(id="nick_name"),
-                html.Div(id="grid_container2"),#[grid]
-                html.Div(id="main_panel-12"),
+                html.Div(id='case'),
                 #html.Span('李力, 2024')
             ])
         ]
@@ -130,28 +93,27 @@ clientside_callback(
         return user_Agent
     }
     """,
-    Output("store_12", "data"),
-    Input("store_12", "data"),
+    Output("store_13", "data"),
+    Input("store_13", "data"),
 )
 
 @app1.callback(
-    Output("input1", "value"),
-    Output("output-image-database", "src"),
-    Output("radio_items", "value"),
-    Input('url', 'pathname'))
-def update_input(pathname):
-    def get_client_id():
+    Output("case", "children"),
+    Input('url', 'pathname'),
+    Input("store_13", "data")
+    )
+def update_client_info(pathname, store_13):
+    def get_client_info():
+        client_info = {'client_id': [], 'field1': [], 'field2': []}
         if session.get('token'):
             token = session.get('token')
             #print('token: ', token)
-            username = session.get('username')
-            #print('username: ', username)
             home_url = 'https://pocketbase-5umc.onrender.com' #'http://127.0.0.1:8090/'
             # 使用已经登录获取到的token，查询client_id
             get_path = '/api/collections/clients/records'
 
-            query_client_id = "?filter=(username='" + username + "'||email='" + username + "')&&fields=id"#&&page=50&&perPage=100&&date&&skipTotal=1response1_json
-            get_url = home_url + get_path + query_client_id
+            query_client_info = "?filter=(field4=True)&&fields=id,field1,field2"#&&page=50&&perPage=100&&date&&skipTotal=1response1_json
+            get_url = home_url + get_path + query_client_info
             header = {
                 "Content-Type": "application/json",
                 "Authorization": token
@@ -161,72 +123,19 @@ def update_input(pathname):
             response_str = str(response_json)
             logger.debug('response_str: {}'.format(response_str[0:100]))
             #print('response_str: {}'.format(response_str[0:100]))
-            client_id = response_json['items'][0]['id']
-        return client_id
-    client_id = get_client_id()
-    home_url = 'https://pocketbase-5umc.onrender.com' #'http://127.0.0.1:8090/'
-
-    if session.get('token'):
-        token = session.get('token')
-        logger.debug('token: {}'.format(token))
-        # 使用已经登录获取到的token 发送一个get请求
-        get_path = '/api/collections/clients/records'
-
-        query_field1 = "?filter=(id='" + client_id + "')&&fields=field1,field2,field4&&perPage=1&&page=1"# + str(i)&&page=50&&perPage=100&&sort=date&&skipTotal=1response1_json
-        get_url = home_url + get_path + query_field1
-        header2 = {
-            "Content-Type": "application/json",
-            "Authorization": token
-        }
-        response4 = requests.get(get_url, headers=header2)
-        response4_json = response4.json()
-        response4_str = str(response4_json)
-        logger.debug('response4_str: {}'.format(response4_str[0:100]))
-        if response4_json['totalItems'] > 0:
-            field1 = response4_json['items'][0]['field1']
-            field2 = response4_json['items'][0]['field2']
-            field4 = response4_json['items'][0]['field4']
-            logger.debug('field1: {}, field4: {}'.format(field1, field4))
-            if field4 == True:
-                get_radio_items = "公开"
-            else:
-                get_radio_items = "不公开"
-    return field1, field2, get_radio_items   
-
-@app1.callback(
-    Output("output-image-upload", "src"),
-    Input("upload-image", "contents"),
-)
-def update_output_image(list_of_contents):
-    if list_of_contents is not None:
-        logger.debug('list_of_contents: {}'.format(list_of_contents[0:10]))
-    return list_of_contents
-@app1.callback(
-        Output("nick_name", "children"),
-        Output("grid_container2", "children"),
-        Output("main_panel-12", "children"),
-        Input("output-image-upload", "src"),
-        Input("output-image-database", "src"),
-        Input("radio_items", "value"),
-        Input("input1", "value"),
-        Input("store_12", "data"),
-        Input("button_12", "n_clicks")
-    )
-def update_my_share(upload_contents, database_contents, radio_items, input1, store_12, n_clicks):
-    ## if button is clicked
-    if n_clicks > 0:
-        contents = ""
-        if upload_contents is not None:
-            logger.debug('upload_contents: {}'.format(upload_contents[0:10]))
-            contents = upload_contents
-        elif database_contents is not None:
-            logger.debug('database_contents: {}'.format(database_contents[0:10]))
-            contents = database_contents
-        logger.debug('contents: {}'.format(contents[0:10]))
-
+            if response_json['totalItems'] > 0:
+                for item in response_json['items']:
+                    client_info['client_id'].append(item['id'])
+                    client_info['field1'].append(item['field1'])
+                    client_info['field2'].append(item['field2'])
+        return client_info
+    if pathname == '/case':
+        client_info = get_client_info()
+        client_info_df = pd.DataFrame(client_info)
+        
         nick_name = html.Div([
-            html.Div([html.Img(src=contents, style={'width': '50px', 'height': '50px'})],style={'padding': 10, 'flex': 1}),
-            html.Div([html.H3(input1)], style={'padding': 10, 'flex': 1}),
+            html.Div([html.Img(src=client_info['field2'][0], style={'width': '50px', 'height': '50px'})],style={'padding': 10, 'flex': 1}),
+            html.Div([html.H3(client_info['field1'][0])], style={'padding': 10, 'flex': 1}),
             html.Div("    ",style={'padding': 10, 'flex': 1}),
             html.Div("    ",style={'padding': 10, 'flex': 1}),
             html.Div("    ",style={'padding': 10, 'flex': 1}),
@@ -241,32 +150,8 @@ def update_my_share(upload_contents, database_contents, radio_items, input1, sto
             #html.Div("    ",style={'padding': 10, 'flex': 1}),
         ], style={'display': 'flex', 'flexDirection': 'row'})
         TIMEOUT = 60 * 60 * 24
-        def get_client_id():
-            if session.get('token'):
-                token = session.get('token')
-                #print('token: ', token)
-                username = session.get('username')
-                #print('username: ', username)
-                home_url = 'https://pocketbase-5umc.onrender.com' #'http://127.0.0.1:8090/'
-                # 使用已经登录获取到的token，查询client_id
-                get_path = '/api/collections/clients/records'
-
-                query_client_id = "?filter=(username='" + username + "'||email='" + username + "')&&fields=id"#&&page=50&&perPage=100&&date&&skipTotal=1response1_json
-                get_url = home_url + get_path + query_client_id
-                header = {
-                    "Content-Type": "application/json",
-                    "Authorization": token
-                }
-                response = requests.get(get_url, headers=header)
-                response_json = response.json()
-                response_str = str(response_json)
-                logger.debug('response_str: {}'.format(response_str[0:100]))
-                #print('response_str: {}'.format(response_str[0:100]))
-                client_id = response_json['items'][0]['id']
-            return client_id
-        client_id = get_client_id() 
         @cache.memoize(timeout=TIMEOUT)
-        def get_my_total_return_client2(frequency = 'daily',client_id=''):
+        def get_my_total_return_client3(frequency = 'daily',client_id=''):
             home_url = 'https://pocketbase-5umc.onrender.com' #'http://127.0.0.1:8090/'
 
             if session.get('token'):
@@ -329,39 +214,8 @@ def update_my_share(upload_contents, database_contents, radio_items, input1, sto
                 data = [generate_random_series(5000, n=500), generate_random_series(5000, n=500)]
 
             return data
-
                 
-        if radio_items  == "公开":
-            is_open = True
-        else:
-            is_open = False
-        if session.get('token'):
-            token = session.get('token')
-            #print('token: ', token)
-            home_url = 'https://pocketbase-5umc.onrender.com' #'http://127.0.0.1:8090/'
-            # 使用已经登录获取到的token，发送数据
-            patch_path = '/api/collections/clients/records/' + client_id
-            data = {
-                'field1':input1,
-                'field2': contents,
-                'field4': is_open,
-            }
-            data_json = json.dumps(data)
-            patch_url = home_url + patch_path
-            header = {
-                "Content-Type": "application/json",
-                "Authorization": token
-            }
-            response = requests.patch(patch_url, headers=header, data=data_json)
-            response_json = response.json()
-            response_str = str(response_json)
-            if response.status_code == 200:
-                logger.debug('client_id: {}, post successful.'.format(client_id))
-            else:
-                logger.debug('response_str: {}'.format(response_str[0:100]))
-            #print('response_str: {}'.format(response_str[0:100]))
-    
-        data0 = get_my_total_return_client2(frequency='daily',client_id=client_id)
+        data0 = get_my_total_return_client3(frequency='daily',client_id=client_info['client_id'][0])
         data_annualized_return = data0[1]
         #df1 = pd.DataFrame(data_annualized_return)
         #df2_1 = df1[['annualized_return ', 'annualized_volatility']]
@@ -409,7 +263,7 @@ def update_my_share(upload_contents, database_contents, radio_items, input1, sto
         logger.debug('data0[1]: {}'.format(str(data0[1])[0:50]))
 
         main_panel = [
-            html.H2('我的累计收益率图 📊'),
+            #html.H2('我的累计收益率图 📊'),
             #html.H3('根据历史经验，比特币市值偏差为1时，比特币市值在牛市顶部，计算出的比特币价格为牛市的价格上限，比特币市值偏差为-0.95时，比特币市值在熊市底部，计算出的比特币价格为熊市的价格下限。'),
 
             html.Div(style={'position': 'relative', 'width': '100%', 'height': '100%', 'marginBottom': '30px'}, children=[
@@ -447,7 +301,7 @@ def update_my_share(upload_contents, database_contents, radio_items, input1, sto
                         },
                         seriesOptions=[
                             {
-                                'title': '我的累计收益率',
+                                'title': '累计收益率',
                                 #'color': 'blue' 
                                 #'priceScaleId': 'left'
                             }
@@ -460,17 +314,16 @@ def update_my_share(upload_contents, database_contents, radio_items, input1, sto
                 ], style={'position': 'absolute', 'left': 0, 'top': 0, 'zIndex': 10, 'color': 'white', 'padding': '10px'})
             ])
         ]
-        user_agent = parse(store_12)
+        user_agent = parse(store_13)
         is_mobile = user_agent.is_mobile
         is_tablet = user_agent.is_tablet
         is_pc = user_agent.is_pc
-        if radio_items == "公开":
-            if is_pc:
-                return nick_name, [grid1], main_panel
-            elif is_mobile or is_tablet:
-                return nick_name, [grid2_1, grid2_2], main_panel
-        else:
-            raise PreventUpdate
-    else:
-        raise PreventUpdate
+
+        if is_pc:
+            return [html.Div(nick_name), html.Div([grid1]), html.Div(main_panel), html.Hr()]
+        elif is_mobile or is_tablet:
+            return [html.Div(nick_name), html.Div([grid2_1, grid2_2]), html.Div(main_panel), html.Hr()]
+            #nick_name, [grid2_1, grid2_2], main_panel
+ 
+
     
